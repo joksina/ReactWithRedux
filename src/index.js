3576740788d50224a40af37627be707b421c3a99
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-07 16:06:25 -0700

    Working with React
    By Stephen

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//get access to react
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+//create a new companents and should produce some html
+const App =  () => {
+  // body...
+  return <div>HI World!</div>;
+};
+
+//take the component and render on the DOM page
+
+ReactDOM.render(<App />, document.querySelector('.container'));
