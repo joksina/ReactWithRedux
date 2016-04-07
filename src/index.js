839558219d72801f84c734289f0f788cff6a5a32
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-07 16:38:36 -0700

    search bar rendering on DOM

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 //get access to react
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDp8yw8o87bc7Z822sNzgrWFocoOmBJXHQ';
 
 //create a new companents and should produce some html
 const App =  () => {
   // body...
-  return <div>HI World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 };
 
 //take the component and render on the DOM page
