3b63458b2f22ef7c344727aab67287d5c40e4cf9
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-08 19:19:40 -0700

    DOM looking good

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 //get access to react
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDp8yw8o87bc7Z822sNzgrWFocoOmBJXHQ';
 
+
 //create a new companents and should produce some html
-const App =  () => {
-  // body...
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+      //this.setState({ videos: videos }); but in ES6
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
 
 //take the component and render on the DOM page
 
