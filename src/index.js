8ab898b95c65955164247c6b880be0026d60e13a
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-10 01:00:49 -0700

    importing the list to the application page

--- src/index.js
+++ src/index.js
@@ -13,20 +13,32 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surf')
+  }
 
-    YTSearch({key: API_KEY, term: 'surf'}, (videos) => {
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       //this.setState({ videos: videos }); but in ES6
-      this.setState({ videos });
-    });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });   
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
